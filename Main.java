b8a15676111a7fba312b35368d3a93e4e76ccf6d
baleh <youcef.baleh@etu.u-paris.fr>
2025-01-10 01:01:20 +0100

    change

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-//create a web server that is handling apis like this https://zululu/?account_verification_report_id=c0bb217f7a2a4882a0094fbd73c43d08
+//create a web server that is handling apis like this https://zululu.netlify.app/?account_verification_report_id=c0bb217f7a2a4882a0094fbd73c43d08
 // the server will be deployed in netlify 
-
-
+// use a tomcat server
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.Map;
 
 
 import com.sun.net.httpserver.HttpExchange;
@@ -17,8 +14,8 @@ import com.sun.net.httpserver.HttpServer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/zululu", new MyHandler());
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/api", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
@@ -26,26 +23,11 @@ public class Main {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            Map<String, String> params = queryToMap(t.getRequestURI().getQuery());
             String response = "This is the response";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
         }
-    }
-
-    public static Map<String, String> queryToMap(String query) {
-        Map<String, String> result = new HashMap<>();
-        for (String param : query.split("&")) {
-            String pair[] = param.split("=");
-            if (pair.length > 1) {
-                result.put(pair[0], pair[1]);
-            } else {
-                result.put(pair[0], "");
-            }
-        }
-        // un comment 
-        return result;
     }
 }

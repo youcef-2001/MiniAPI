12a90f7247471f878dd82df84b8192ee3b31c17f
baleh <youcef.baleh@etu.u-paris.fr>
2025-01-10 01:55:37 +0100

    port http

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   const accountVerificationReportId = req.query.account_verification_report_id;
